ed5f4eddb040abbf944a649d22f2d3aad70d65fc
Phoebe <jolinhappy1990@hotmail.com>
2020-07-24 18:56:27 +0800

    feat: add mongoose

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 3000
+
+// mongoose.connect('mongodb://localhost/todo-list-new', { useNewUrlParser: true }, { useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+
+// const exphbs = require('express-handlebars')
+// const bodyParser = require('body-parser')
+
+
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+// app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.send('hello world!')
+})
+
+app.listen(port, () => {
+  console.log('OK')
+})
---
+++ models/todo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  }
+})
+
+module.exports = mongoose.model('Todo', todoSchema)
+
+//這裡的Todo是資料名字，別的地方直接使用Todo
